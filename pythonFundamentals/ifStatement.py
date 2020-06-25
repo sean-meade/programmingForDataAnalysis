43895c82ea5ac558510ae4836f8d50f592b9cd16
seanp <seanpatmeade@gmail.com>
2020-06-25 08:56:38 +0100

    Written out expalin if statement.

--- pythonFundamentals/ifStatement.py
+++ pythonFundamentals/ifStatement.py
@@ -1,1 +1,13 @@
 #This explains IF statements
+# Import module
+import datetime
+
+today = datetime.datetime.today()
+dayofweek = today.weekday()
+
+if dayofweek == 1:
+    print("It's Tuesday!")
+
+else:
+    print("Unfortunately it's not Tuesday.")
+
