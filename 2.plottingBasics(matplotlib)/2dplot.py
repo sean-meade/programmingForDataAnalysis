369f259ffdf66d47e581d05087d995bc171f7d44
seanp <seanpatmeade@gmail.com>
2020-06-29 10:59:44 +0100

    Added second lecture folder. Completed basics in matplotlib example.

---
+++ 2.plottingBasics(matplotlib)/2dplot.py
@@ -0,0 +1,12 @@
+# Go to the following link you can hover and click ctrl+left mouse key to open a new tab with
+# the documentation for that function.
+# https://matplotlib.org/tutorials/introductory/pyplot.html
+# Use matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
+
+plt.plot([1, 2, 3, 4])
+plt.ylabel("Some numbers")
+plt.show()
+
+plt.plot([1, 2, 3, 4], [1, 4, 9, 16], "b.")
+plt.show()
